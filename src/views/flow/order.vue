1b6688323f38b058ef9b9f8b6ff9f5d83d3c7908
imjeen <imjeen@sina.com>
2016-01-19 22:04:44 +0800

    update: font-size and cart page

--- src/views/flow/order.vue
+++ src/views/flow/order.vue
@@ -3,22 +3,22 @@
 </style>
 
 <template>
-	<h1>我的订单</h1>
-	<section v-for="orderItem in orderItems"> 
-		<h2>{{$index + 1}}xx订单 - 店铺<span>等待买家付款</span></h2>
-		<a v-link="{ path: 'list/' + ($index + 1) + '/detail' }">
-			<ul>
-				<li><img  alt=""></li>
-			</ul>
-		</a>
-		<p>下单时间：</p>
-		<p>共3个商品，总价30￥</p>
-		<p>
-			<button @click.prevent="doCancelOrder('hello')">取消订单</button>
-			<!-- go to a url -->
-			<button @click.prevent="doOrderIt">支付</button>
-		</p>
+
+	<nav>
+		<ul class="i-grid">
+			<li class="i-cell i-cell--2-col"><a href="#">全部</a></li>
+			<li class="i-cell i-cell--2-col"><a href="#">待支付</a></li>
+			<li class="i-cell i-cell--2-col"><a href="#">待发货</a></li>
+			<li class="i-cell i-cell--2-col"><a href="#">待收货</a></li>
+			<li class="i-cell i-cell--2-col"><a href="#">待评价</a></li>
+			<li class="i-cell i-cell--2-col"><a href="#">退款/售后</a></li>
+		</ul>
+	</nav>
+
+	<section class="normal-layer">
+		
 	</section>
+
 </template>
 
 <script>
@@ -27,24 +27,9 @@
 		// data
 		data(){
 			return {
-				orderNumber: 100,
-				orderItems: [
-					{
-						orderId: 1
-					},
-					{
-						orderId: 2
-					}
-				]
 			}
 		},
 		methods: {
-			doCancelOrder(msg){
-				alert('cancelOrder...' + msg);
-			},
-			doOrderIt(){
-				alert("order it!")
-			}
 		},
 		route: {
 			data(transition){
